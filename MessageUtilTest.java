cf2f0d26b0609089c7d270bd0650a8918fab0f9a
Nahom Wondimu <148879888+nwondim2@users.noreply.github.com>
2024-05-31 21:52:33 -0400

    Add files via upload

---
+++ MessageUtilTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MessageUtilTest {
+	
+   String message = "Hello World";	
+   MessageUtil messageUtil = new MessageUtil(message);
+
+   @Test
+   public void testPrintMessage() {
+      assertEquals(message,messageUtil.printMessage());
+   }
+}
